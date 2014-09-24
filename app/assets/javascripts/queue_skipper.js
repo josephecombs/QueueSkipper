b5b138e04f90d05634bd3feebe50487894fa4e33
Joseph Combs <joseph.e.combs@gmail.com>
2014-09-24 09:23:21 -0700

    some backbone setup

---
+++ app/assets/javascripts/queue_skipper.js
@@ -0,0 +1,17 @@
+window.QueueSkipper = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    new QueueSkipper.Routers.Router({
+      $rootEl: $("#content")
+    });
+    alert('Hello from Backbone!');
+    Backbone.history.start();
+  }
+};
+
+$(document).ready(function(){
+  QueueSkipper.initialize();
+});
