27011b720882ce05252f7272cde38b54cf696e92
Joseph Combs <joseph.e.combs@gmail.com>
2014-09-30 11:27:32 -0700

    added a bit more styling, starying to implement new listing functionality

---
+++ app/assets/javascripts/views/listings/new_listing.js
@@ -0,0 +1,19 @@
+QueueSkipper.Views.NewListingView = Backbone.View.extend({
+  initialize: function(){
+    this.mapView = new QueueSkipper.Views.MapView({
+      collection: this.collection
+    }).render();
+    this.listingsIndex = new QueueSkipper.Views.ListingsIndex({
+      collection: this.collection
+    }).render();
+  },
+  
+  template: JST['listings/new'],
+  
+  render: function(){
+    this.$el.html(this.template);
+    this.$('#map').html(this.mapView.$el);
+    this.$('#listings-index').html(this.listingsIndex.$el);
+    return this;
+  },
+});
