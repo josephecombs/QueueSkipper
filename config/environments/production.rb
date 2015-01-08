88f421636561ad9aa00ed17880ac69e89502587b
Joseph Combs <joseph.e.combs@gmail.com>
2015-01-08 00:01:35 -0800

    more email config issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {:host => 'queueskipper.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "127.0.0.1",
-    :port    => 25,
-    :domain  => 'queueskipper.com'
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
   }
 end
